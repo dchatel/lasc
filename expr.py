92e47aa61ac3c28559cb8d69d7a9caa340582c94
David Chatel <dchatel@live.fr>
2014-05-06 21:31:50 +0200

    expr

--- expr.py
+++ expr.py
@@ -5,7 +5,10 @@ operator_precedence = {
 	'div': 2,
 	'schur': 2,
 	'add': 3,
-	'sub': 3
+	'sub': 3,
+	'neg': 3,
+	'kron': 2,
+	'vec': 0
 }
 operator_repr = {
 	'var': '',
@@ -14,94 +17,114 @@ operator_repr = {
 	'div': ' / ',
 	'schur': ' * ',
 	'add': ' + ',
-	'sub': ' - '
+	'sub': ' - ',
+	'neg': '-',
+	'kron': u' \u2297 ',
+	'vec': 'vec'
 }
+operator_arity = {
+	'var': 1,
+	'pow': 2,
+	'mul': 2,
+	'div': 2,
+	'schur': 2,
+	'add': 2,
+	'sub': 2,
+	'neg': 1,
+	'kron': 2,
+	'vec': 1
+}
+def scalar(name): pass
 
 class expr(object):
-	def __init__(self, operator, operands, negative=False):
+	def __init__(self, operator, symbol, precedence, rows=scalar('n'), cols=scalar('m'), symmetric=False):
 		self._operator = operator
-		self._operands = []
-		for i in range(len(operands)):
-			if isinstance(operands[i], expr) and operands[i].operator == self.operator:
-				for j in range(len(operands[i].operands)):
-					self._operands.append(operands[i].operands[j])
-			else:
-				self._operands.append(operands[i])
-		self._negative = negative
+		self._symbol = symbol
+		self._precedence = precedence
+		self._rows = rows
+		self._cols = cols
+		self._symmetric = symmetric
 
 	@property
-	def operator(self):
-	    return self._operator
-	@operator.setter
-	def operator(self, value):
-	    self._operator = value
-
+	def operator(self): return self._operator
 	@property
-	def operands(self):
-	    return self._operands
-	@operands.setter
-	def operands(self, value):
-	    self._operands = value
-
+	def symbol(self): return self._symbol
 	@property
-	def negative(self):
-	    return self._negative
-	@negative.setter
-	def negative(self, value):
-	    self._negative = value
-
+	def precedence(self): return self._precedence
 	@property
-	def precedence(self):
-	    return operator_precedence[self.operator]
-	
-	def __add__(self, other):
-		return expr('add', [self, other])
-	def __radd__(self, other):
-		return expr('add', [other, self])
-	def __sub__(self, other):
-		return expr('sub', [self, other])
-	def __rsub__(self, other):
-		return expr('sub', [other, self])
+	def rows(self): return self._rows
+	@property
+	def cols(self): return self._cols	
+	@property
+	def symmetric(self): return self._symmetric	
+
 	def __mul__(self, other):
-		return expr('mul', [self, other])
-	def __rmul__(self, other):
-		return expr('mul', [other, self])
-	def __div__(self, other):
-		return expr('div', [self, other])
-	def __rdiv__(self, other):
-		return expr('div', [other, self])
-	def __pow__(self, other):
-		return expr('pow', [self, other])
-	def __mod__(self, other):
-		return expr('schur', [self, other])
-	def __rmod__(self, other):
-		return expr('schur', [other, self])
-	def __pos__(self):
-		return expr(self.operator, self.operands, self.negative)
-	def __neg__(self):
-		return expr(self.operator, self.operands, self.negative == False)
+		return mult(self,other)
+
+class unaryexpr(expr):
+	def __init__(self, operator, symbol, precedence, operand, rows=scalar('n'), cols=scalar('m'), symmetric=False):
+		expr.__init__(self, operator, symbol, precedence, rows=rows, cols=cols, symmetric=symmetric)
+		self._operand = operand
+
+	@property
+	def operand(self): return self._operand
 
 	def __str__(self):
-		s = operator_repr[self.operator].join(map(lambda x: '(' + x.__str__() + ')' if isinstance(x, expr) and x.precedence >= self.precedence else x.__str__(), self.operands))
-		if self.negative == True:
-			if self.precedence > 0:
-				return '-(' + s + ')'
-			else:
-				return '-' + s
+		if isinstance(self.operand, expr) and self.operand.precedence >= self.precedence:
+			return '%s(%s)' % (self.symbol, self.operand.__str__())
 		else:
-			return s
-	def __repr__(self):
-		s = '(' + self.operator + ' ' + ' '.join(map(lambda x: x.__repr__(), self.operands)) + ')'
-		if self.negative == True:
-			return '-' + s
-		else:
-			return s
+			return '%s%s' % (self.symbol, self.operand.__str__())
 
-class var(expr):
-	def __init__(self, name):
-		expr.__init__(self, 'var', [name], False)
-	#	
-	#def __repr__(self):
-	#	return self.name
-	#def __str__(self):
-	#	return self.name
+	def __repr__(self):
+		return '(%s[%sx%s%s] %s)' % (self.operator, self.rows, self.cols, ' Sym' if self.symmetric == True else '', self.operand.__repr__())
+
+	def __eq__(self, other):
+		return self.operator == other.operator and self.operand == other.operand
+
+class binaryexpr(expr):
+	def __init__(self, operator, symbol, precedence, left, right, rows=scalar('n'), cols=scalar('m'), symmetric=False):
+		expr.__init__(self, operator, symbol, precedence, rows=rows, cols=cols, symmetric=symmetric)
+		self._left = left
+		self._right = right
+
+	@property
+	def left(self): return self._left
+	@property
+	def right(self): return self._right
+	
+	def __str__(self):
+		if isinstance(self.left, expr) and self.left.precedence >= self.precedence:
+			s = '(%s)%s' % (self.left.__str__(), self.symbol)
+		else:
+			s = '%s%s' % (self.left.__str__(), self.symbol)
+		if isinstance(self.right, expr) and self.right.precedence >= self.precedence:
+			s = '%s(%s)' % (s, self.right.__str__())
+		else:
+			s = '%s%s' % (s, self.right.__str__())
+		return s
+
+	def __repr__(self):
+		return '(%s[%sx%s%s] %s %s)' % (self.operator, self.rows, self.cols, ' Sym' if self.symmetric == True else '', self.left.__repr__(), self.right.__repr__())
+
+def var(name):
+	return unaryexpr('var', '', 0, name)
+
+def matrix(name, rows=scalar('n'), cols=scalar('m'), symmetric=False):
+	return unaryexpr('var', '', 0, name, rows=rows, cols=cols, symmetric=symmetric)
+
+def vector(name, rows=scalar('n')):
+	return unaryexpr('var', '', 0, name, rows=rows, cols=1, symmetric=False)
+
+def scalar(name):
+	return unaryexpr('var', '', 0, name, rows=1, cols=1, symmetric=True)
+
+def vec(e):
+	return unaryexpr('vec', 'vec', 0, e, rows=e.rows * e.cols, cols=1, symmetric=False)
+
+def t(e):
+	return unaryexpr('transpose', 't', 0, e, rows=e.cols, cols=e.rows, symmetric=e.symmetric)
+
+def mult(left, right):
+	assert left.cols == right.rows, 'Operator mult, matrices '+left.__repr__()+' and '+right.__repr__()+' are incompatible'
+	# TODO Symmetric if left == t(right)
+	return binaryexpr('mult', '', 1, left, right, rows=left.rows, cols=right.cols, symmetric=False)

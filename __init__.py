6f5539826af27b4843674d0312dab23197e434a3
David Chatel <david.chatel@inria.fr>
2014-05-16 16:33:40 +0200

    plpl

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+from collections import namedtuple
+
 class LascException(Exception):
 	def __init__(self, message):
 		self._message = message
@@ -31,10 +33,8 @@ class expr(object):
 
 	def __hash__(self):
 		return hash(self.__repr__())
-		return hash(hashing().hash(self))
 	def __eq__(self, other):
-		return hashing().hash(self) == hashing().hash(other)
-		#return matcher().equality(self, other)
+		return identic(self, other)
 	def __ne__(self, other):
 		return not self.__eq__(other)
 
@@ -62,7 +62,7 @@ class unaryexpr(expr):
 			return '%s%s' % (self.symbol, self.operand.__str__())
 
 	def __repr__(self):
-		return '(%s[%sx%s] %s)' % (self.operator, self.rows, self.cols, self.operand.__repr__())
+		return u'(%s[%s\u00d7%s] %s)' % (self.operator, self.rows, self.cols, self.operand.__repr__())
 
 class binaryexpr(expr):
 	def __init__(self, operator, symbol, precedence, left, right, rows=scalar('n'), cols=scalar('m')):
@@ -87,22 +87,16 @@ class binaryexpr(expr):
 		return s
 
 	def __repr__(self):
-		return '(%s[%sx%s] %s %s)' % (self.operator, self.rows, self.cols, self.left.__repr__(), self.right.__repr__())
+		return u'(%s[%s\u00d7%s] %s %s)' % (self.operator, self.rows, self.cols, self.left.__repr__(), self.right.__repr__())
 
 def scalar(name):
 	return unaryexpr('var', '', 0, name, rows=1, cols=1)
-#def scalartok(name):
-#	return unaryexpr('tok', '', 0, name, rows=1, cols=1)
 
 def vector(name, rows=scalar('n')):
 	return unaryexpr('var', '', 0, name, rows=rows, cols=1)
-#def vectortok(name, rows=scalartok('n')):
-#	return unaryexpr('tok', '', 0, name, rows=rows, cols=1)
 
 def matrix(name, rows=scalar('n'), cols=scalar('m')):
 	return unaryexpr('var', '', 0, name, rows=rows, cols=cols)
-#def matrixtok(name, rows=scalartok('n'), cols=scalartok('m')):
-#	return unaryexpr('tok', '', 0, name, rows=rows, cols=cols)
 
 def vec(e):
 	return unaryexpr('vec', 'vec', 0, e, rows=e.rows * e.cols, cols=1)
@@ -111,141 +105,109 @@ def t(e):
 	return unaryexpr('transpose', 't', 0, e, rows=e.cols, cols=e.rows)
 
 def add(left, right):
-	if isinstance(left, expr) and isinstance(right, expr):
-		if left.cols != right.cols or left.rows != right.rows:
-			raise LascException('Operator add, incompatible matrices:\n%s\n%s' % (left.__repr__(), right.__repr__()))
+	if isinstance(left, (int, long, float)): left = scalar(left)
+	if isinstance(right, (int, long, float)): right = scalar(right)
+
+	if is_scalar(left) or is_scalar(right):
+		return binaryexpr('add', '+', 2, left, right, rows=max(left.rows, right.rows), cols=max(left.cols, right.cols))
+	elif left.rows == right.rows and left.cols == right.cols:
 		return binaryexpr('add', '+', 2, left, right, rows=left.rows, cols=left.cols)
-	elif isinstance(left, expr) and isinstance(right, (int, long, float)):
-		return binaryexpr('add', '+', 2, left, right, rows=left.rows, cols=left.cols)
-	elif isinstance(right, expr) and isinstance(left, (int, long, float)):
-		return binaryexpr('add', '+', 2, left, right, rows=right.rows, cols=right.cols)
-	else:
-		raise LascException('Invalid operation `add` between %s and %s' % (left.__repr__(), right.__repr__()))
+
+	raise LascException('Operator add, incompatible matrices:\n%s\n%s' % (left.__repr__(), right.__repr__()))
 
 def mult(left, right):
-	if isinstance(left, expr) and isinstance(right, expr):
-		if left.cols != right.rows:
-			raise LascException('Operator mult, incompatible matrices:\n%s\n%s' % (left.__repr__(),right.__repr__()))
+	if isinstance(left, (int, long, float)): left = scalar(left)
+	if isinstance(right, (int, long, float)): right = scalar(right)
+
+	if is_scalar(left) or is_scalar(right):
+		return binaryexpr('mult', '', 1, left, right, rows=max(left.rows, right.rows), cols=max(left.cols, right.cols))
+	elif left.cols == right.rows:
 		return binaryexpr('mult', '', 1, left, right, rows=left.rows, cols=right.cols)
-	elif isinstance(left, expr) and isinstance(right, (int, long, float)):
-		return binaryexpr('mult', '', 1, left, right, rows=left.rows, cols=left.cols)
-	elif isinstance(right, expr) and isinstance(left, (int, long, float)):
-		return binaryexpr('mult', '', 1, left, right, rows=right.rows, cols=right.cols)
-	else:
-		raise LascException('Invalid operation `mult` between %s and %s' % (left.__repr__(), right.__repr__()))
 
-class hashing(object):
-	def __init__(self):
-		super(hashing, self).__init__()
-		self.dic = {}
+	raise LascException('Operator mult, incompatible matrices:\n%s\n%s' % (left.__repr__(),right.__repr__()))
 
-	def hash(self, e):
-		if isinstance(e, unaryexpr) and e.operator == 'var':
-			if e.operand not in self.dic:
-				self.dic[e.operand] = len(self.dic)
-			return '(%s[%sx%s] %s)' % (e.operator, self.hash(e.rows), self.hash(e.cols), self.dic[e.operand])
-		elif isinstance(e, unaryexpr):
-			return '(%s[%sx%s] %s)' % (e.operator, self.hash(e.rows), self.hash(e.cols), self.hash(e.operand))
-		elif isinstance(e, binaryexpr):
-			return '(%s[%sx%s] %s %s)' % (e.operator, self.hash(e.rows), self.hash(e.cols), self.hash(e.left), self.hash(e.right))
-		else:
-			return e.__repr__()
+def is_scalar(e):
+	return isinstance(e, expr) and e.rows == 1 and e.cols == 1
 
 def identic(a, b):
 	if isinstance(a, unaryexpr) and isinstance(b, unaryexpr):
-		return identic(a.rows, b.rows) and \
+		return a.operator == b.operator and \
+		identic(a.rows, b.rows) and \
 		identic(a.cols, b.cols) and \
 		identic(a.operand, b.operand)
 	elif isinstance(a, binaryexpr) and isinstance(b, binaryexpr):
-		return identic(a.rows, b.rows) and \
+		return a.operator == b.operator and \
+		identic(a.rows, b.rows) and \
 		identic(a.cols, b.cols) and \
 		identic(a.left, b.left) and \
 		identic(a.right, b.right)
 	else:
-		return a == b
+		return not isinstance(a, expr) and not isinstance(b, expr) and a == b
 
-def match(pattern, e):
+def match_exact(pattern, e):
+	Answer = namedtuple('Answer','matched map')
 	dic = {}
 
 	def internal(pattern, e):
 		equal = True
 		# check valued scalars
-		if isinstance(pattern, (int, long, float)) and isinstance(e, (int, long, float)): equal = pattern == e
+		if isinstance(pattern, (int, long, float)) and isinstance(e, (int, long, float)):
+			equal = pattern == e
 		# check sizes
 		elif isinstance(pattern, (int, long, float)) and isinstance(e, expr) and not(internal(e.rows, 1) and internal(e.cols, 1)):
-			print 'expression %s not scalar' % e
 			equal = False
 		elif isinstance(e, (int, long, float)) and isinstance(pattern, expr) and not(internal(pattern.rows, 1) and internal(pattern.cols, 1)):
-			print 'pattern %s not scalar' % pattern
 			equal = False
 		elif isinstance(pattern, expr) and isinstance(e, expr) and not(internal(pattern.rows, e.rows) and internal(pattern.cols, e.cols)):
-			print 'wrong size for %s and %s' % (pattern, e)
 			equal = False
 
 		elif pattern in dic:
-			print '%s found in dic' % pattern
 			equal = identic(dic[pattern], e)
 
 		elif isinstance(pattern, unaryexpr):
-			print '%s is unary' % pattern.__repr__()
 			if pattern.operator == 'var':
-				print 'Let %s = %s' % (pattern, e)
 				dic[pattern] = e
 				equal = True
 			else: equal = pattern.operator == e.operator and internal(pattern.operand, e.operand)
 		else: equal = isinstance(pattern, binaryexpr) and isinstance(e, binaryexpr) and pattern.operator == e.operator and internal(pattern.left, e.left) and internal(pattern.right, e.right)
-		print '%s %s %s' % (pattern, '=' if equal else u'\u2260', e)
 		return equal
 
 	answer = internal(pattern, e)
-	return (answer, dic)
+	return Answer(answer, dic)
 
-#class matcher(object):
-#	def __init__(self):
-#		super(matcher, self).__init__()
-#		self.dic = {}
-#
-#	def equality(self, a, b, showproof=False, depth=0):
-#		if showproof:
-#			print '%sTesting %s = %s' % ((' ' * depth), a, b)
-#		if a in self.dic:
-#			if showproof:
-#				print '%sRecall %s = %s' % ((' ' * depth), a, self.dic[a])
-#			return self.equality(self.dic[a], b, showproof, depth+2)
-#		if b in self.dic:
-#			if showproof:
-#				print '%sRecall %s = %s' % ((' ' * depth), b, self.dic[b])
-#			return self.equality(self.dic[b], a, showproof, depth+2)
-#		if a not in self.dic and isinstance(a, unaryexpr) and a.operator == 'tok' and (isinstance(b, (int, long, float)) and (self.equality(a.rows, 1, showproof, depth+2) and self.equality(a.cols, 1, showproof, depth+2)) or (isinstance(b, expr) and b.operator != 'tok' and self.equality(b.rows, a.rows, showproof, depth+2) and self.equality(b.cols, a.cols, showproof, depth+2))):
-#			if showproof:
-#				print '%sLet %s = %s' % ((' ' * depth), a, b)
-#			self.dic[a] = b
-#			return True
-#		if b not in self.dic and isinstance(b, unaryexpr) and b.operator == 'tok' and (isinstance(a, (int, long, float)) and (self.equality(b.rows, 1, showproof, depth+2) and self.equality(b.cols, 1, showproof, depth+2)) or (isinstance(a, expr) and a.operator != 'tok' and self.equality(a.rows, b.rows, showproof, depth+2) and self.equality(a.cols, b.cols, showproof, depth+2))):
-#			if showproof:
-#				print '%sLet %s = %s' % ((' ' * depth), b, a)
-#			self.dic[b] = a
-#			return True
-#		if isinstance(a, unaryexpr) and a.operator == 'tok' and isinstance(b, unaryexpr) and b.operator == 'tok' and a.operand == b.operand:
-#			if showproof:
-#				print '%s%s = %s' % ((' ' * depth), a, b)
-#			return True
-#		if isinstance(a, (int, long, float)) and isinstance(b, (int, long, float)):
-#			if showproof:
-#				print '%s%s %s %s' % ((' ' * depth), a, '=' if a == b else u'\u2260', b)
-#			return a == b
-#		if isinstance(a, unaryexpr) and a.operator == 'var' and isinstance(b, unaryexpr) and b.operator == 'var' and a.operand == b.operand:
-#			if showproof:
-#				print '%s%s = %s' % ((' ' * depth), a, b)
-#			return True
-#		if isinstance(a, unaryexpr) and isinstance(b, unaryexpr) and a.operator == b.operator and self.equality(a.operand, b.operand, showproof, depth+2):
-#			if showproof:
-#				print '%s%s = %s' % ((' ' * depth), a, b)
-#			return True
-#		if isinstance(a, binaryexpr) and isinstance(b, binaryexpr) and a.operator == b.operator and self.equality(a.left, b.left, showproof, depth+2) and self.equality(a.right, b.right, showproof, depth+2):
-#			if showproof:
-#				print '%s%s = %s' % ((' ' * depth), a, b)
-#			return True
-#		if showproof:
-#			print u'%s%s \u2260 %s' %((' ' * depth), a, b)
-#		return False
+def count(pattern, e):
+	total = 1 if match_exact(pattern, e).matched else 0
+	if isinstance(e, unaryexpr): total += count(pattern, e.operand)
+	elif isinstance(e, binaryexpr): total += count(pattern, e.left) + count(pattern, e.right)
+	return total
+
+def matchall(pattern, e):
+	matches = []
+
+	def internal(pattern, e):
+		m = match_exact(pattern, e)
+		if m.matched: matches.append(m.map)
+		if isinstance(e, unaryexpr): internal(pattern, e.operand)
+		elif isinstance(e, binaryexpr):
+			internal(pattern, e.left)
+			internal(pattern, e.right)
+
+	internal(pattern, e)
+	return matches
+
+def rewrite(rewrited, dic):
+	if rewrited in dic:
+		return dic[rewrited]
+	elif isinstance(rewrited, unaryexpr):
+		return unaryexpr(rewrited.operator, rewrited.symbol, rewrited.precedence, rewrite(rewrited.operand, dic), rows=rewrite(rewrited.rows, dic), cols=rewrite(rewrited.cols, dic))
+	elif isinstance(rewrited, binaryexpr):
+		return unaryexpr(rewrited.operator, rewrited.symbol, rewrited.precedence, rewrite(rewrited.left, dic), rewrite(rewrited.right, dic), rows=rewrite(rewrited.rows, dic), cols=rewrite(rewrited.cols, dic))
+
+def reduceall(original, rewrited, e):
+	m = match_exact(original, e)
+	if m.matched:
+			return rewrite(rewrited, m)
+	if isinstance(e, unaryexpr):
+		return unaryexpr(e.operator, e.symbol, e.precedence, reduceall(original, rewrited, e.operand), reduceall(original, rewrited, e.rows), reduceall(original, rewrited, e.cols))
+	elif isinstance(e, binaryexpr):
+		return binaryexpr(e.operator, e.symbol, e.precedence, reduceall(original, rewrited, e.left), reduceall(original, rewrited, e.right), reduceall(original, rewrited, e.rows), reduceall(original, rewrited, e.cols))
